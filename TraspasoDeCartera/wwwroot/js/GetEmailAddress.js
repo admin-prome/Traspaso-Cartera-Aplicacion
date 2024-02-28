87e2d905db73fd5856e1436b22caf2cd43176029
SantiagoMartinezProme <samartinez@provinciamicrocreditos.com>
2024-02-28 12:04:13 -0300

    getEmail

---
+++ TraspasoDeCartera/wwwroot/js/GetEmailAddress.js
@@ -0,0 +1,33 @@
+﻿export async function getEmailAdress() {
+    try {
+        // Make a GET request to the /.auth/me endpoint
+        const response = await fetch("https://traspaso-cartera.azurewebsites.net/.auth/me", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                // Add any additional headers as needed
+            },
+        });
+        console.log(response);
+        if (response.ok) {
+            // Parse the JSON response
+            const userData = await response.json();
+
+            // Extract the email address
+            const emailClaim = userData.find(claim => claim.typ === "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+
+            if (emailClaim) {
+                return emailClaim.val;
+            } else {
+                console.error("Email address claim not found in response.");
+                return null;
+            }
+        } else {
+            console.error(`Failed to fetch email address: ${response.status} ${response.statusText}`);
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching email address:", error);
+        return null;
+    }
+}
